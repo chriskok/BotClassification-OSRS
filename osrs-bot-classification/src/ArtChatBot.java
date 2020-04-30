2920f262e2a54ef0d2aa30c973b85c8a68cd8e04
chriskok <ckok@purdue.edu>
2020-04-30 07:56:05 -0500

    Finished art chat bot

--- osrs-bot-classification/src/ArtChatBot.java
+++ osrs-bot-classification/src/ArtChatBot.java
@@ -17,59 +17,61 @@ public class ArtChatBot extends AbstractScript {
     }
 
     // Art received from https://textart.io/art/tag/small/1
-    private String[] pig = new String[]{"     ,--.   ,--.\n",
+    private String[] pig = new String[]{"oink!",
+            "     ,--.   ,--.\n",
             "     \\  /~\\  /\n",
             "      )' a a `(\n",
             "     (  ,---.  )\n",
-            "      ( o_o)'\n",
-            "        )`-'( hjw"};
-    private String[] duck = new String[]{
-            "         ,~~.\n",
+            "       (o_o)'\n",
+            "        )`-'(   hjw"};
+    private String[] duck = new String[]{"quack!",
+            "          ,~~.\n",
             "          (  9 )-_,\n",
             "(\\__ )='-'\n",
-            " \\ .    )  )\n",
-            "  \\ `-' /\n",
-            "   `~-'   hjw\n"};
-    private String[] cat = new String[]{
-            "     A_A\n",
-            "    (-.-)\n",
-            "  | - |\n",
-            " /   \\\n",
-            "|       |   __\n",
-            "|    || |  |  \\__\n",
+            " \\ .      )  )\n",
+            "  \\ `-'   /\n",
+            "   `~-  '   hjw\n"};
+    private String[] cat = new String[]{"meow!",
+            "  A..A\n",
+            " (-.- )\n",
+            "  | -  |\n",
+            " /     \\\n",
+            "|         \\     _\n",
+            "|     ||    |    |  \\__\n",
             " \\_||_/_/"};
-    private String[] owl = new String[]{
-            " /\\_/\\\n",
+    private String[] owl = new String[]{"hoot!",
+            "   /\\_/\\\n",
             "((@v@))\n",
-            "():::()\n",
-            " VV-VV"};
+            "()  :::::::  ()\n",
+            "     V-V"};
 
     public void printAnimal(int animalIndex) {
-        log("animal chosen: " + animalIndex);
-        for (String strTemp : owl){
+//        log("animal chosen: " + animalIndex);
+
+        for (String strTemp : pig){
             getKeyboard().type(strTemp, true);
-            sleep(Calculations.random(2000, 3000));
+            sleep(Calculations.random(3000, 4000));
+        }
+
+        sleep(Calculations.random(10000, 20000));
+
+        for (String strTemp : duck){
+            getKeyboard().type(strTemp, true);
+            sleep(Calculations.random(3000, 4000));
+        }
+
+        sleep(Calculations.random(10000, 20000));
+
+        for (String strTemp : cat){
+            getKeyboard().type(strTemp, true);
+            sleep(Calculations.random(3000, 4000));
         }
 
         sleep(Calculations.random(10000, 20000));
 
         for (String strTemp : owl){
             getKeyboard().type(strTemp, true);
-            sleep(Calculations.random(1500, 2000));
-        }
-
-        sleep(Calculations.random(10000, 20000));
-
-        for (String strTemp : owl){
-            getKeyboard().type(strTemp, true);
-            sleep(Calculations.random(2000, 3000));
-        }
-
-        sleep(Calculations.random(10000, 20000));
-
-        for (String strTemp : owl){
-            getKeyboard().type(strTemp, true);
-            sleep(Calculations.random(1500, 2000));
+            sleep(Calculations.random(3000, 4000));
         }
         sleep(Calculations.random(10000, 20000));
 
